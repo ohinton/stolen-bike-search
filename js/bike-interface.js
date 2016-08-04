5ef33c2550cca293f8819a449ad1ac0f248fe64c
Olivia Hinton and Rachael Bermudes <olivia@>
2016-08-04 11:54:47 -0700

    working on printing out results

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,1 +1,9 @@
 var Bike = require('./../js/bike.js').bikeModule;
+
+$(document).ready(function() {
+  var currentBikeObject = new Bike();
+  $('#inputLocation').click(function() {
+    var city = $('#location').val();
+    currentBikeObject.getBikesByLocation(city);
+    });
+  });
--- js/bike.js
+++ js/bike.js
@@ -2,8 +2,15 @@ function Bike(){
 
 }
 
-Bike.prototype.searchResults = function() {
-
+Bike.prototype.getBikesByLocation = function(city) {
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + city + "&proximity_square=100").then(function(response) {
+    response.bikes.forEach(function(bike) {
+      var title = response.bikes.title;
+      $('.showResults').append("<li>" + title + "</li>");
+      }).fail(function(error){
+      $('#showBikes').text(error.responseJSON.message);
+      });
+  });
 };
 
 exports.bikeModule = Bike;
