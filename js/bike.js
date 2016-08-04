2550883473821e24c4e359b3e2f60575dcaf320b
Olivia Hinton and Rachael Bermudes <olivia@>
2016-08-04 14:26:07 -0700

    add same styling to page

--- js/bike.js
+++ js/bike.js
@@ -6,7 +6,8 @@ Bike.prototype.getBikesByLocation = function(city) {
   $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + city + "&proximity_square=100").then(function(response) {
     response.bikes.forEach(function(bike) {
       var title = bike.title;
-      $('.showResults').append("<li>" + title + "</li>");
+      var manufacturer_name = bike.manufacturer_name;
+      $('.showResults').append("<li>" + title + " " + manufacturer_name + "</li>");
       }).fail(function(error){
       $('#showResults').text(error.responseJSON.message);
       });
